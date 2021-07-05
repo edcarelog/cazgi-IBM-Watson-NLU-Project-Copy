25853b63365fe4e805ffd35a8bbdb93522845a19
Edi Alfred <edcarelog@aol.com>
2021-07-04 23:45:36 -0400

    commit "second commit"
    closed

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import './bootstrap.min.css';
+const axios = require('axios').default;
+
 
 class EmotionTable extends React.Component {
-    render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
+  state = { eventList:[] }
+  
+  componentDidMount() {
+    let url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/b5711716-691c-4f19-b047-354de52919be"
+    const req = axios.get(url);
+    console.log(req);
+
+  req.then(resp => {
+    let listOfEvents = resp.data.authorEvent;
+    let listOfEventsAsArray = Object.entries(listOfEvents);
+    let eventDetails = listOfEventsAsArray.map((eventDetial)=>{
+    let eventListCollection = Object.entries(eventDetial[1])
+
+    return <tr><td style={{color: "red",border: "1px solid black"}}>{eventListCollection[4][1]} </td>
+    <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[5][1]} </td>
+    <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[7][1]}</td>
+    <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[10][1]}</td>
+    <td style={{color: "red",border: "1px solid black"}}> {eventListCollection[11][1]}</td></tr>
+
+    })
+
+    this.setState({eventList:<table style={{border: "1px solid black"}}>
+      <tbody>{eventDetails}</tbody>
+      </table>})
+      })
+      .catch(err => {
+        console.log(err.toString())
+      });
+  }
+
+render() {
+  const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+    // eslint-disable-next-line no-unused-vars
+    let li_ctr = 0;  
+  // eslint-disable-next-line no-lone-blocks
+  {JSON.stringify(this.props.emotions)}     
+  return ( 
+    <div style={colorStyle}>
+       Events List
+        <br/>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+            this.state.eventList
             }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+    </div>                                       
+  );
+}
 }
 export default EmotionTable;
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,47 @@
 const express = require('express');
 const app = new express();
-
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get("/url/emotion", (req,res) => {
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
 
-    return res.send({"happy":"90","sad":"10"});
-});
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
-});
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
-});
-
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
-});
-
-let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
-
+    app.get("/url/emotion", (req,res) => {
+        return res.send("Response about emotion"+req.query.emotion);
+    });
+    
+    app.get("/url/sentiment", (req,res) => {
+        return res.send("Response about sentiment"+req.query.sentiment);
+    });
+    
+    app.get("/text/emotion", (req,res) => {
+        return res.send("Response about emotion"+req.query.emotion);
+    });
+    
+    app.get("/text/sentiment", (req,res) => {
+        return res.send("Response about sentiment"+req.query.sentiment);
+    });
+       
+    
+    app.listen(8080, () => {
+        console.log(`Listening at http://localhost:8080`)
+    })
